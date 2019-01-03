c8ae71e14a48d505adeb2a2d599b1f12298e64d6
Timothe <t.lafranche@rubika-edu.com>
2019-01-03 16:54:33 +0100

    Scorpide V0 (Pas Fini)

---
+++ ProtoZeldaLike/Assets/ItsTheFirstProto/Scripts/Scorpide/ScorpideComportement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScorpideComportement : MonoBehaviour {
+
+    public bool isAsleep = true;
+
+    public Sprite asleepSprite;
+    public Sprite wakeUpSprite;
+
+    public float attackDmg;
+
+    public float speed;
+
+    Vector2 velocity;
+    Rigidbody2D rigiBoy;
+
+    public Transform target;
+
+    public GameObject weapon;
+
+    public float walkDistance;
+
+    public List<Transform> canReachTarget;
+
+    private void Start()
+    {
+        rigiBoy = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if(Vector2.Distance(target.position,transform.position) <= walkDistance || isAsleep)
+        {
+            velocity = Vector2.zero;
+        }
+        else if(target != null)
+        {
+            velocity = (target.position - transform.position).normalized * speed;
+        }
+
+        if(target == null && !isAsleep)
+        {
+            target = canReachTarget[Random.Range(0, canReachTarget.Count - 1)];
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rigiBoy.MovePosition(rigiBoy.position + velocity * Time.fixedDeltaTime);
+    }
+
+    public void WakeUp(Transform whoWakeUp)
+    {
+        GetComponent<SpriteRenderer>().sprite = wakeUpSprite;
+        target = whoWakeUp;
+    }
+
+    public void Attack()
+    {
+        weapon.SetActive(true);
+        weapon.transform.up = target.position - transform.position;
+    }
+}
---
+++ ProtoZeldaLike/Assets/ItsTheFirstProto/Scripts/Scorpide/ScorpideVision.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScorpideVision : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag =="Player" || collision.tag == "Ennemis" || collision.tag == "Creature")
+        {
+
+        }
+    }
+}
